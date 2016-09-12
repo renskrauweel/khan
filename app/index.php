e78b8f7abce75db7a1c24aff3623f88dcb4ca771
Nils <192600@edu.rocfriesepoort.nl>
2016-09-12 15:34:50 +0200

    HTML CSS start

---
+++ app/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/style.css" type="text/css" >
+	<title>Khan Board</title>
+</head>
+<body>
+	<div class="logo"><img src="images/logo_plain.png" alt="logo"></div>
+
+</body>
+</html>
