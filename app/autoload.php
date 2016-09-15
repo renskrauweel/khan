53a67f22f54251d2a208f09d8da849ecd58e8eac
Rens Krauweel <renskrauweel@hotmail.com>
2016-09-15 14:39:25 +0200

    Updated autoloader

--- app/autoload.php
+++ app/autoload.php
@@ -1,8 +1,5 @@
 <?php
 
 	require_once("classes/Leaderboard.class.php");
-	require_once("classes/LeaderboardStudents.class.php");
-	require_once("classes/LeaderboardClasses.class.php");
-	require_once("classes/LeaderboardClassroom.class.php");
 
 ?>
