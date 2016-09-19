e4adae76de7aad243fe75b489e14417fa0a7ec39
Rens Krauweel <renskrauweel@hotmail.com>
2016-09-19 15:21:39 +0200

    Included database

---
+++ app/leaderboard.sql
@@ -0,0 +1,34 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server versie:                10.1.8-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Versie:              9.3.0.4984
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Databasestructuur van leaderboard wordt geschreven
+CREATE DATABASE IF NOT EXISTS `leaderboard` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+USE `leaderboard`;
+
+
+-- Structuur van  tabel leaderboard.leaderboard wordt geschreven
+CREATE TABLE IF NOT EXISTS `leaderboard` (
+  `id` int(11) NOT NULL,
+  `course` varchar(255) COLLATE utf8_bin NOT NULL,
+  `description` varchar(255) COLLATE utf8_bin NOT NULL,
+  `first` varchar(255) COLLATE utf8_bin NOT NULL,
+  `second` varchar(255) COLLATE utf8_bin NOT NULL,
+  `third` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Dumpen data van tabel leaderboard.leaderboard: ~0 rows (ongeveer)
+/*!40000 ALTER TABLE `leaderboard` DISABLE KEYS */;
+/*!40000 ALTER TABLE `leaderboard` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
