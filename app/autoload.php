767b78a695c6bb7079f0b72ebf957d8aeefafc4c
Nils <192600@edu.rocfriesepoort.nl>
2016-09-20 14:23:37 +0200

    Further changes

--- app/autoload.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-	require_once("classes/Leaderboard.class.php");
-
-?>
--- app/index.php
+++ app/index.php
@@ -2,10 +2,32 @@
 <html>
 <head>
 	<link rel="stylesheet" href="css/style.css" type="text/css" >
+	 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script src="javascripts/jquery.easing.1.3.js"></script>
+  <script src="javascripts/jquery.animate-enhanced.min.js"></script>
+  <script src="dist/jquery.superslides.js" type="text/javascript" charset="utf-8"></script>
+  <script>
+    $(function() {
+      $('#slides').superslides({
+        hashchange: true
+      });
+    });
+  </script>
 	<title>Khan Board</title>
 </head>
 <body>
 	<div class="logo"><img src="images/logo_plain.png" alt="logo"></div>
+	 <div id="slides">
+    <div class="slides-container">
+      <div class="single-slide"><h1>I403A</h1></div>
+      <div class="single-slide"><h1>I403B</h1></div>
+     </div>
+
+    <nav class="slides-navigation">
+      <a href="#" class="next">Next</a>
+      <a href="#" class="prev">Previous</a>
+    </nav>
+  </div>
 
 </body>
 </html>
