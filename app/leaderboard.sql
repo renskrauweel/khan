3d5a4c425d70f5b03f5fac4a1a1b969a0414923b
Rens Krauweel <renskrauweel@hotmail.com>
2016-09-20 13:52:47 +0200

    Added Auto Increment to DB

--- app/leaderboard.sql
+++ app/leaderboard.sql
@@ -17,17 +17,20 @@ USE `leaderboard`;
 
 -- Structuur van  tabel leaderboard.leaderboard wordt geschreven
 CREATE TABLE IF NOT EXISTS `leaderboard` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `course` varchar(255) COLLATE utf8_bin NOT NULL,
   `description` varchar(255) COLLATE utf8_bin NOT NULL,
   `first` varchar(255) COLLATE utf8_bin NOT NULL,
   `second` varchar(255) COLLATE utf8_bin NOT NULL,
   `third` varchar(255) COLLATE utf8_bin NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- Dumpen data van tabel leaderboard.leaderboard: ~0 rows (ongeveer)
 /*!40000 ALTER TABLE `leaderboard` DISABLE KEYS */;
+INSERT INTO `leaderboard` (`id`, `course`, `description`, `first`, `second`, `third`, `date`) VALUES
+	(1, 'engels', 'Leerlingen all time', 'Kris', 'Yousef Abdull Jabar (nils)', 'Rens Krauweel', '2016-09-20 13:50:43');
 /*!40000 ALTER TABLE `leaderboard` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
