1e273d7a31d6c016785ee1132fe4bd8b9062535e
Rens Krauweel <renskrauweel@hotmail.com>
2016-10-04 16:10:19 +0200

    Updated db

--- session.sql
+++ session.sql
@@ -1,62 +1,27 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Gegenereerd op: 04 okt 2016 om 13:41
--- Serverversie: 5.7.11
--- PHP-versie: 5.6.18
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `leaderboard`
---
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server versie:                10.1.8-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Versie:              9.3.0.4984
 -- --------------------------------------------------------
 
---
--- Tabelstructuur voor tabel `session`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE TABLE `session` (
-  `id` int(11) NOT NULL,
-  `session` text COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- Structuur van  tabel leaderboard.session wordt geschreven
+CREATE TABLE IF NOT EXISTS `session` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Gegevens worden geëxporteerd voor tabel `session`
---
-
+-- Dumpen data van tabel leaderboard.session: ~0 rows (ongeveer)
+/*!40000 ALTER TABLE `session` DISABLE KEYS */;
 INSERT INTO `session` (`id`, `session`) VALUES
-(3, 'a:1:{s:22:"oauth_5xLdMmpejfNeYvbw";a:10:{s:12:"consumer_key";s:16:"5xLdMmpejfNeYvbw";s:15:"consumer_secret";s:16:"r5czsqpG5cUsXW9K";s:17:"signature_methods";a:1:{i:0;s:9:"HMAC-SHA1";}s:10:"server_uri";s:27:"https:dit teken kan er niet inwww.khanacademy.org";s:17:"request_token_uri";s:50:"https:dit teken kan er niet inwww.khanacademy.org/api/auth/request_token";s:13:"authorize_uri";s:46:"https:dit teken kan er niet inwww.khanacademy.org/api/auth/authorize";s:16:"access_token_uri";s:49:"https:dit teken kan er niet inwww.khanacademy.org/api/auth/access_token";s:10:"token_type";s:6:"access";s:5:"token";s:17:"t5185247058460672";s:12:"token_secret";s:16:"hEc5NARkfrYnPuAy";}}');
-
---
--- Indexen voor geëxporteerde tabellen
---
-
---
--- Indexen voor tabel `session`
---
-ALTER TABLE `session`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT voor geëxporteerde tabellen
---
-
---
--- AUTO_INCREMENT voor een tabel `session`
---
-ALTER TABLE `session`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+	(4, 'a:2:{s:22:"oauth_5xLdMmpejfNeYvbw";a:10:{s:12:"consumer_key";s:16:"5xLdMmpejfNeYvbw";s:15:"consumer_secret";s:16:"r5czsqpG5cUsXW9K";s:17:"signature_methods";a:1:{i:0;s:9:"HMAC-SHA1";}s:10:"server_uri";s:27:"https:dit teken kan er niet inwww.khanacademy.org";s:17:"request_token_uri";s:50:"https:dit teken kan er niet inwww.khanacademy.org/api/auth/request_token";s:13:"authorize_uri";s:46:"https:dit teken kan er niet inwww.khanacademy.org/api/auth/authorize";s:16:"access_token_uri";s:49:"https:dit teken kan er niet inwww.khanacademy.org/api/auth/access_token";s:10:"token_type";s:6:"access";s:5:"token";s:17:"t5185247058460672";s:12:"token_secret";s:16:"hEc5NARkfrYnPuAy";}s:22:"oauth_YgdmKf7XedvH9UH7";a:10:{s:12:"consumer_key";s:16:"YgdmKf7XedvH9UH7";s:15:"consumer_secret";s:16:"BRgsfmrcKTgJ9qUm";s:17:"signature_methods";a:1:{i:0;s:9:"HMAC-SHA1";}s:10:"server_uri";s:27:"https:dit teken kan er niet inwww.khanacademy.org";s:17:"request_token_uri";s:50:"https:dit teken kan er niet inwww.khanacademy.org/api/auth/request_token";s:13:"authorize_uri";s:46:"https:dit teken kan er niet inwww.khanacademy.org/api/auth/authorize";s:16:"access_token_uri";s:49:"https:dit teken kan er niet inwww.khanacademy.org/api/auth/access_token";s:10:"token_type";s:6:"access";s:5:"token";s:17:"t5107147037605888";s:12:"token_secret";s:16:"CubAwDKAmKvuqVpF";}}');
+/*!40000 ALTER TABLE `session` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
