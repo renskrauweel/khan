68919848114e78f786e3e246403e15ae74a5a1a4
Rens Krauweel <renskrauweel@hotmail.com>
2016-10-06 11:05:56 +0200

    Updated db

--- app/leaderboard.sql
+++ app/leaderboard.sql
@@ -25,13 +25,38 @@ CREATE TABLE IF NOT EXISTS `leaderboard` (
   `third` varchar(255) COLLATE utf8_bin NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=216 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Dumpen data van tabel leaderboard.leaderboard: ~0 rows (ongeveer)
+-- Dumpen data van tabel leaderboard.leaderboard: ~11 rows (ongeveer)
 /*!40000 ALTER TABLE `leaderboard` DISABLE KEYS */;
 INSERT INTO `leaderboard` (`id`, `course`, `description`, `first`, `second`, `third`, `date`) VALUES
-	(1, 'engels', 'Leerlingen all time', 'Kris', 'Yousef Abdull Jabar (nils)', 'Rens Krauweel', '2016-09-20 13:50:43');
+	(204, 'engels', 'Alle leerlingen', 'Kris', 'Yousef Abdull Jabar (nils)', 'Rens Krauweel', '2016-10-04 16:05:57'),
+	(205, 'Rekenen', 'I4O3A', 'Rens Krauweel', '', '', '2016-10-04 16:05:57'),
+	(206, 'Rekenen', 'I4O2A', 'dijkstra.eric', '', '', '2016-10-04 16:05:57'),
+	(207, 'Rekenen', 'I4O1A', 'Yousef Abdull Jabar (nils)', 'Rens Krauweel', '', '2016-10-04 16:05:57'),
+	(208, 'engels', 'Alle leerlingen', 'Kris', 'Yousef Abdull Jabar (nils)', 'Rens Krauweel', '2016-10-04 16:09:07'),
+	(209, 'Rekenen', 'I4O3A', 'Rens Krauweel', '', '', '2016-10-04 16:09:07'),
+	(210, 'Rekenen', 'I4O2A', 'dijkstra.eric', '', '', '2016-10-04 16:09:07'),
+	(211, 'Rekenen', 'I4O1A', 'Yousef Abdull Jabar (nils)', 'Rens Krauweel', '', '2016-10-04 16:09:07'),
+	(212, 'engels', 'Alle leerlingen', 'Kris', 'Yousef Abdull Jabar (nils)', 'Rens Krauweel', '2016-10-04 16:17:21'),
+	(213, 'Rekenen', 'I4O3A', 'Rens Krauweel', '', '', '2016-10-04 16:17:21'),
+	(214, 'Rekenen', 'I4O2A', 'dijkstra.eric', '', '', '2016-10-04 16:17:21'),
+	(215, 'Rekenen', 'I4O1A', 'Yousef Abdull Jabar (nils)', 'Rens Krauweel', '', '2016-10-04 16:17:21');
 /*!40000 ALTER TABLE `leaderboard` ENABLE KEYS */;
+
+
+-- Structuur van  tabel leaderboard.session wordt geschreven
+CREATE TABLE IF NOT EXISTS `session` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Dumpen data van tabel leaderboard.session: ~1 rows (ongeveer)
+/*!40000 ALTER TABLE `session` DISABLE KEYS */;
+INSERT INTO `session` (`id`, `session`) VALUES
+	(4, 'a:2:{s:22:"oauth_5xLdMmpejfNeYvbw";a:10:{s:12:"consumer_key";s:16:"5xLdMmpejfNeYvbw";s:15:"consumer_secret";s:16:"r5czsqpG5cUsXW9K";s:17:"signature_methods";a:1:{i:0;s:9:"HMAC-SHA1";}s:10:"server_uri";s:27:"https:dit teken kan er niet inwww.khanacademy.org";s:17:"request_token_uri";s:50:"https:dit teken kan er niet inwww.khanacademy.org/api/auth/request_token";s:13:"authorize_uri";s:46:"https:dit teken kan er niet inwww.khanacademy.org/api/auth/authorize";s:16:"access_token_uri";s:49:"https:dit teken kan er niet inwww.khanacademy.org/api/auth/access_token";s:10:"token_type";s:6:"access";s:5:"token";s:17:"t5185247058460672";s:12:"token_secret";s:16:"hEc5NARkfrYnPuAy";}s:22:"oauth_YgdmKf7XedvH9UH7";a:10:{s:12:"consumer_key";s:16:"YgdmKf7XedvH9UH7";s:15:"consumer_secret";s:16:"BRgsfmrcKTgJ9qUm";s:17:"signature_methods";a:1:{i:0;s:9:"HMAC-SHA1";}s:10:"server_uri";s:27:"https:dit teken kan er niet inwww.khanacademy.org";s:17:"request_token_uri";s:50:"https:dit teken kan er niet inwww.khanacademy.org/api/auth/request_token";s:13:"authorize_uri";s:46:"https:dit teken kan er niet inwww.khanacademy.org/api/auth/authorize";s:16:"access_token_uri";s:49:"https:dit teken kan er niet inwww.khanacademy.org/api/auth/access_token";s:10:"token_type";s:6:"access";s:5:"token";s:17:"t5107147037605888";s:12:"token_secret";s:16:"CubAwDKAmKvuqVpF";}}');
+/*!40000 ALTER TABLE `session` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
