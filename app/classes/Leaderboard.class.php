4d08bcf7ffb4f1c836b503f5d58d7245f6df39c1
EricDijkstra <dijkstra.eric@gmail.com>
2016-10-13 11:18:48 +0200

    Moved function ka_client>leaderboard

--- app/classes/Leaderboard.class.php
+++ app/classes/Leaderboard.class.php
@@ -36,6 +36,8 @@ EOT
 
 			return ($data);
 		}
+ 
+
 
 		public static function getStudentsAlltime($resultObject)
 		{
@@ -67,6 +69,54 @@ EOT
 	        VALUES ("Rekenen", "Alle leerlingen", "{$positions[0]}", "{$positions[1]}", "{$positions[2]}")
 EOT
 	        );
+		}
+
+		public static function insertStudentsByClass($studentsByClass) {
+			foreach ($studentsByClass as $class => $students) {
+                arsort($students);
+                $description = $class;
+                $counter = 1;
+                $first = "";
+                $second = "";
+                $third = "";
+                foreach ($students as $student => $badgeCount) {
+                    switch ($counter) {
+                        case 1:
+                            if (!empty($student)) {
+                                $first = $student;
+                            }
+                            break;
+                        case 2:
+                            if (!empty($student)) {
+                                $second = $student;
+                            }
+                            break;
+                        case 3:
+                            if (!empty($student)) {
+                                $third = $student;
+                            }
+                            break;
+                        
+                        default:
+                            break;
+                    }
+                    $counter++;
+                }
+                /*
+                    echo "<b>Description: {$description}</b><br>";
+                    echo "First: {$first}<br>";
+                    echo "Second: {$second}<br>";
+                    echo "Third: {$third}<br>";
+                    */
+                    //Query
+                    $mysqli=DB::get();
+                    $result=$mysqli->query(<<<EOT
+                    INSERT INTO leaderboard (course, description, first, second, third)
+                    VALUES ("Rekenen", "{$description}", "{$first}", "{$second}", "{$third}")
+EOT
+                    );
+            }
+           
 		}
 
 		public static function sortByClass($file)
