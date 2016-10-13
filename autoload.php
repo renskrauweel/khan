849a1a512d2ce1b2058f061225c91d2ba0853634
renskrauweel <renskrauweel@hotmail.com>
2016-10-13 11:48:42 +0200

    Restructured classes

--- autoload.php
+++ autoload.php
@@ -2,4 +2,7 @@
 	require_once("app/db.php");
 	require_once("app/classes/Leaderboard.class.php");
 
+	require_once("modules/module.class.php");
+	require_once("modules/rekenen/classes/rekenmodule.class.php");
+
 ?>
